41ac574bcdedd3ca1760b66fbf42302af7265cbe
Sandeep Kumar Shah <82407422+Sandeep96Shah@users.noreply.github.com>
2026-07-25 10:52:35 +0530

    Time: 0 ms (100%), Space: 52.3 MB (97.43%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    const wordList = s.split(" ");
+    for(let i = wordList.length - 1; i >= 0; i--){
+        if(wordList[i]){
+            return wordList[i].length
+        }
+    }
+};
