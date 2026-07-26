8edb160808c7ca6dfc03c314b1eceeaead23e228
Sandeep Kumar Shah <82407422+Sandeep96Shah@users.noreply.github.com>
2026-07-26 12:14:05 +0530

    Time: 2 ms (79.94%), Space: 58.4 MB (67.06%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -5,24 +5,23 @@
  */
 var insert = function(intervals, newInterval) {
     const n = intervals.length;
-    intervals.push(newInterval);
-    
-    intervals = intervals.sort((a, b) => a[0] - b[0]);
-    console.log("Intervals", intervals);
     const result = [];
-    let currentResult = intervals[0];
-    let start = 1;
-    while(start < n+1){
-        if(intervals[start][0] <= currentResult[1]){
-            currentResult = [currentResult[0], Math.max(currentResult[1], intervals[start][1])];
-            // console.log("CurrentResult", currentResult);
-        }else{
-            result.push(currentResult);
-            // console.log("Inside Loop Result", result);
-            currentResult = intervals[start];
-        }
-        start++;
+    let i = 0;
+    while(i < n && intervals[i][1] < newInterval[0]){
+        result.push(intervals[i]);
+        i++;
     }
-    result.push(currentResult);
+
+    while(i < n && intervals[i][0] <= newInterval[1]){
+        newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+        newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+        i++;
+    }
+    result.push(newInterval);
+
+    while(i < n){
+        result.push(intervals[i]);
+        i++;
+    }
     return result;
 };
