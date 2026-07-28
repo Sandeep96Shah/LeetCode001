0715d807ecf7cf2b9f33103e48818fc74eea612c
Sandeep Kumar Shah <82407422+Sandeep96Shah@users.noreply.github.com>
2026-07-28 09:46:50 +0530

    Time: 81 ms (66.93%), Space: 80 MB (25.33%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    points = points.sort((a, b) => {
+       if(a[0] != b[0]) return a[0] - b[0]
+       return a[1] - b[1];
+}
+);
+    let count = 1;
+    let start = 1;
+    let current = points[0];
+    while(start < points.length){
+        if(points[start][0] <= current[1]){
+            current[0] = Math.max(current[0], points[start][0]);
+            current[1] = Math.min(current[1], points[start][1]);
+            start++;
+            continue;
+        }else{
+            current = points[start];
+            count++;
+        }
+        start++;
+    }
+    return count;
+};
