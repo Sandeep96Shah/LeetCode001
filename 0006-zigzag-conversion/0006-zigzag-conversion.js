decca7132b9672d7e9d5275cdc69f03c3effbf71
Sandeep Kumar Shah <82407422+Sandeep96Shah@users.noreply.github.com>
2026-07-29 21:12:34 +0530

    Time: 442 ms (5.03%), Space: 81.2 MB (5.14%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    console.log({s: s.length})
+    const n = s.length;
+    const arr = Array.from({ length: numRows }, () => []);
+    let count = 0;
+    let col = 0;
+    while(count < n){
+        let row = 0;
+        while(row < numRows){
+            arr[row][col] = s[count];
+            console.log(row, col, s[count]);
+            count++;
+            row++;
+        }
+        col++;
+        row = numRows - 2;
+        while(row > 0 && col < n){
+            arr[row][col] = s[count];
+            console.log(row, col, s[count]);
+            row--;
+            col++;
+            count++;
+        }
+    }
+    let str = "";
+    for(let i = 0; i < numRows; i++){
+        for(let j = 0; j < n; j++){
+            if(arr[i][j]){
+                str = str + arr[i][j];
+            }
+        }
+    }
+    return str;
+};
