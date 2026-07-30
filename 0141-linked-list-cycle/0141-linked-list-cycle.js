52c1b1ba3c25164bc9e8cb2c4e9ddf694309d9c5
Sandeep Kumar Shah <82407422+Sandeep96Shah@users.noreply.github.com>
2026-07-30 18:19:07 +0530

    Time: 58 ms (37.34%), Space: 57.5 MB (91.28%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,13 +11,13 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(head === null) return false;
     let slow = head;
-    let fast = head?.next;
-    while(fast){
-        if(fast === slow) return true;
-        slow = slow?.next;
-        fast = fast?.next?.next;
+    let fast = head;
+    while(fast !== null && fast.next !== null){
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow === fast) return true;
     }
     return false;
 };
